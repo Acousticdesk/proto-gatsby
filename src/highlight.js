43c1946aada83c834e4ee2baec8343bbded534ba
Andrii Kicha <akicha@cogniance.com>
2019-06-14 20:58:33 +0300

    adjust code color scheme

--- src/highlight.js
+++ src/highlight.js
@@ -1,7 +1,13 @@
 import hljs from 'highlight.js/lib/highlight'
 import javascript from 'highlight.js/lib/languages/javascript'
-import 'highlight.js/styles/agate.css'
+import 'highlight.js/styles/foundation.css'
 
+// dark.css
+
+// default.css
+// foundation.css
+
+// googlecode.css
 export const { highlightBlock } = hljs
 
 export default () => hljs.registerLanguage('javascript', javascript)
