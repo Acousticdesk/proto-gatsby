ea618f66ccf264bc72a91a91dfffb2a96ec11d13
Andrii Kicha <akicha@cogniance.com>
2019-06-24 09:51:55 +0300

    add g analytics

--- src/components/meta.js
+++ src/components/meta.js
@@ -1,8 +1,22 @@
+/* eslint-disable react/no-unescaped-entities */
+/* eslint-disable react/jsx-one-expression-per-line */
+
 import React from 'react'
 import Helmet from 'react-helmet'
 
 export default () => (
   <Helmet>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-142558665-1" />
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag() {
+        dataLayer.push(arguments)
+      }
+      gtag('js', new Date());
+
+      gtag('config', 'UA-142558665-1');
+    </script>
+
     <title>letconst - Веб вдохновение</title>
     <meta name="viewport" content="width=device-width,initial-scale=1" />
     <meta name="google-site-verification" content="isIs2HhCzxV5jdc0Na1-2Ke5H64kgvSA3Q-DQGJn3Gg" />
