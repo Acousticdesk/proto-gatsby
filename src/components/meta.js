dea0c1b997b4054b2bf9b815447506cc74b4a727
Andrii Kicha <acousticdesk@gmail.com>
2020-05-31 19:48:47 +0300

    update main website description

--- src/components/meta.js
+++ src/components/meta.js
@@ -7,6 +7,7 @@ import Helmet from 'react-helmet'
 export default () => (
   <Helmet>
     <title>letconst - Highlighting 🔥 Web Development topics</title>
+    <meta name="description" content="If you are interested in Svelte and other web development tool topics, letconst is the right place to land for you!" />
     <meta name="viewport" content="width=device-width,initial-scale=1" />
     <link rel="shortcut icon" href="./assets/favicon.ico" type="image/x-icon" />
   </Helmet>
