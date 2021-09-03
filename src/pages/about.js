6d15aec7a24d5dfb8a8f5fc3ec3ca8bf47db761f
Andrii Kicha <akicha@star.global>
2021-09-03 18:17:45 +0300

    Rename the application in the about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,7 +4,7 @@ import Layout from '../components/layout/index'
 export default () => (
   <Layout>
     <div className="content__section">
-      <h2 className="content__header">Welcome to letconst!</h2>
+      <h2 className="content__header">Welcome to awaitasync!</h2>
     </div>
 
     <div className="content__section">
@@ -13,7 +13,7 @@ export default () => (
         I practice development for 5 years by now and have been working on various projects.
       </p>
       <p>
-        letconst is my attempt to stay closer to Javascript community, to become a better writer,
+        awaitasync is my attempt to stay closer to Javascript community, to become a better writer,
         to develop my own product and of course to gain new friends!
       </p>
       <p>I hope you are having a good time here! Cheers!</p>
